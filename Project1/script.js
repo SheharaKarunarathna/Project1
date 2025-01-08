19d404e62f0e2f2a60b0c58ee3233bd4deef1e87
Nethindu74 <rathnakumaranrnc.23@uom.lk>
2025-01-08 23:09:25 +0530

    js added to about us.
    me hriyta wda krn nanen bn

--- Project1/script.js
+++ Project1/script.js
@@ -2,3 +2,19 @@ const header = document.querySelector("header");
 window.addEventListener("scroll",function(){
     header.classList.toggle("sticky",this.window.scrollY>60)
 });
+    <script>
+        const aboutSection=document.getElementById('about-section')
+        ;
+
+        //content for about us
+
+        const aboutContent= `
+            <h2>About Us</h2>
+            <p>We are a team of 4 passionate members from <strong>University of Moratuwa</strong>, dedicated to making differences through technology and collaboration. </p>
+            <p>Our current project focuses on supporting the '<strong>Scope</strong>' community, aiming to create impactful solutions for real world challenges.</p>
+            <p>With a shared vision and diverse skills, we strive to empower communities through innovation, teamwork and creativity.</p> 
+            
+        `;
+
+        aboutSection.innerHTML=aboutContent;
+    </script>
